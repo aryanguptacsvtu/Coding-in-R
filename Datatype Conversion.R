5c282561e786c4d2fe65c961c2cc109c74d1d887
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-25 21:06:38 +0530

    Datatype Conversion

---
+++ Datatype Conversion.R
@@ -0,0 +1,96 @@
+# Converting Datatypes in R:-
+
+num1 <- as.numeric(23L)  # Conversion into "numeric" datatype
+num1
+
+num2 <- as.numeric(20 + 4i)
+num2
+
+num3 <- as.numeric(TRUE)
+num3
+
+num4 <- as.numeric("HEllo23")
+num4
+
+num5 <- as.numeric("346746")
+num5
+
+
+# Conversion into "integer" datatype
+int1 <- as.integer(354.56) 
+int1
+
+int2 <- as.integer(26 - 4i)
+int2
+
+int3 <- as.integer(TRUE)
+int3
+
+int4 <- as.integer("HEllo23")
+int4
+
+int5 <- as.integer("7889")
+int5
+
+
+# Conversion into "complex" datatype
+comp1 <- as.complex(88) 
+comp1
+
+comp2 <- as.complex(456.444)
+comp2
+
+comp3 <- as.complex(TRUE)
+comp3
+
+comp4 <- as.complex(FALSE)
+comp4
+
+comp5 <- as.complex(55L)
+comp5
+
+comp6 <- as.complex("353")
+comp6
+
+comp7 <- as.complex("hkt67")
+comp7
+
+
+# Conversion into "logical" datatype
+logi1 <- as.logical(88) 
+logi1
+
+logi2 <- as.logical(456.444)
+logi2
+
+logi3 <- as.logical(-54L)
+logi3
+
+logi4 <- as.logical(45 +5i)
+logi4
+
+logi5 <- as.logical(0)
+logi5
+
+logi6 <- as.logical("ngd")
+logi6
+
+logi7 <- as.logical("6464")
+logi7
+
+
+# Conversion into "character" datatype
+char1 <- as.character(100L) 
+char1
+
+char2 <- as.character(456.444)
+char2
+
+char3 <- as.character(34 + 5i)
+char3
+
+char4 <- as.character(TRUE)
+char4
+
+
+
