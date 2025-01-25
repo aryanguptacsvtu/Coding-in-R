572bd3c6b2bd99c4e6ce28992c1259318eac164e
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-25 20:57:34 +0530

    Variables

---
+++ Variables.R
@@ -0,0 +1,22 @@
+#Variables in R programming:-
+
+var_name <- 10 
+var.name <- 20
+.variable_name <- 30
+v5ar_name <- 40
+
+#_var <- 44  {INVALID}
+#7var <- 89
+#var_n*me <- 55
+
+#Assignment of Variables:-
+
+var1<- 1
+var2 = 2
+3 -> var3
+
+print(var1)
+cat(var1," ",var2,"\n")
+cat(var3)
+
+cat("Values : ",var1 , var2 ,var3)
