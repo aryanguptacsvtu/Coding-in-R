0f95a160b22684bcd184a76139109f2a2d2cb002
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-25 21:07:39 +0530

    Operators

---
+++ Operators.R
@@ -0,0 +1,52 @@
+# Operators in R :-
+
+a <- 7.5
+b <- 2
+print(a + b)      # "Arithmetic Operators"
+print(a - b)
+print(a * b)
+print(a / b)      # Division
+print(a %% b)     # Remainder
+print(a %/% b)    # Quotient
+print(a ^ b)      # Power of
+
+
+c1 <- c(8,9,6)    # Vectors
+c2 <- c(2,4,5)
+print(c1 + c2)   
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)      # Division
+print(c1 %% c2)     # Remainder
+print(c1 %/% c2)    # Quotient
+print(c1 ^ c2)      # Power of
+
+
+a <- 7.5
+b <- 2
+print(a < b)     # "Relational Operators"
+print(a <= b)
+print(a > b)
+print(a >= b)
+print(a == b)
+print(a != b)    
+
+
+d <- c(3.6 ,TRUE , 3+5i)
+e <- c(8 ,FALSE , 4-9i)
+print(d & e)       # "Logical Operators"
+print(d | e)
+
+X = 34
+Y = 0
+print(X && Y)
+print(X || Y)
+print(!X)
+print(!Y)
+
+
+var1 <- 10L        # "Assignment Operators"
+var2 <<- 56
+4 + 3i -> var3
+TRUE ->> var4
+var5 = 444
