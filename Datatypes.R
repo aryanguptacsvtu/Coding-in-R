ef89bb816d0fecf746738a856aaaebea393dc74c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-25 21:05:37 +0530

    Datatypes
    Learning about different Datatypes in R...

---
+++ Datatypes.R
@@ -0,0 +1,39 @@
+# Datatypes in R :-
+
+#numeric :=> 12, 34.45, -89, -67.54
+#integer :=> 10L , -33L
+#complex :=> 3+6i ,5-3i
+#logical :=> TRUE , FALSE
+#character :=> 'a' , "HELLO" , '23.34' , "good morning"
+
+#NOTE :- Default datatype is "numeric"
+
+num1 <- 10  #numeric
+class(num1)
+typeof(num1)
+
+num2 <- 10  #numeric
+class(num2)
+typeof(num2)
+
+var <- 32L  #integer
+class(var)
+typeof(var)
+
+a <- -34L   #integer
+class(a)
+typeof(a)
+
+comp <- 55+6i #complex
+class(comp)
+typeof(comp)
+
+logi <- TRUE  #logical
+class(logi)
+typeof(logi)
+
+c <- "hello world"  #character
+d <- 'how are you?'
+e <- "hiii"
+class(c)
+typeof(d)
