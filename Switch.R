e70213481670bd26c61717cffeea27f52d2b93e3
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-26 20:40:20 +0530

    Switch

---
+++ Switch.R
@@ -0,0 +1,21 @@
+# Switch Statement in R :-
+
+x <- switch(2,
+            "Ram",
+            "Shyam",
+            "Arun",
+            "Amit",
+            "Rahul")   # First Way
+
+print(x)
+
+
+a <- "80"
+b <- switch(a,
+            "10"="Ram",
+            "23"="Shyam",
+            "20"="Arun",
+            "80"="Amit",
+            "5"="Rahul")  # Second Way
+
+print(b)
