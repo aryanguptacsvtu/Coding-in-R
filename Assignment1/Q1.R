51cf79d1db7ad7e6fe6f5ea0b14dbbdadfee6a3a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-27 22:58:50 +0530

    Q1

---
+++ Assignment1/Q1.R
@@ -0,0 +1,8 @@
+# QUESTION :-
+#Create and store a sequence of values from 5 to -11 that progresses in steps of 0.3. 
+
+
+a <- seq(5,-11,by=-0.3)  # Generate the sequence and store it in 'a'
+
+# Print the sequence
+print(a)
