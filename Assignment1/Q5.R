52c9a3215dcfb772d60009e6664e21c9be9fc1c0
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-27 23:00:48 +0530

    Q5

---
+++ Assignment1/Q5.R
@@ -0,0 +1,9 @@
+# QUESTION :-
+# Confirm that the length of the vector created in (4.) is 20. 
+
+
+# Recreate the vector from Q4
+vec <- c(c(6:12), rep(5.3, times = 3), -3, seq(102, 100, length.out = 9))
+
+# Print the length of the vector
+print(length(vec))
