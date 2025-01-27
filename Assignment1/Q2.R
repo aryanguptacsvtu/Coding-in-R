e342a0cb88ce15f88bd90d5db30f9ed0eae64a7a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-27 22:59:29 +0530

    Q2

---
+++ Assignment1/Q2.R
@@ -0,0 +1,12 @@
+# QUESTION :- 
+#Overwrite the object from (1.) using the same sequence with the order reversed. 
+
+
+# Reverse the sequence and store it back in 'a'
+a <- rev(a)
+
+# Print the reversed sequence
+print(a)
+
+# Print the length of the reversed sequence
+print(length(a))
