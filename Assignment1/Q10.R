e8a939c76d800a08f44ae63925fc0e95b9f89c95
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-28 22:59:47 +0530

    Q10

---
+++ Assignment1/Q10.R
@@ -0,0 +1,9 @@
+# QUESTION :-
+# Overwrite (6.) with the same values sorted from smallest to largest. 
+
+
+# Sort the vector and overwrite 'vec' with the sorted values
+vec <- sort(vec)
+
+# Print the sorted vector
+print(vec)
