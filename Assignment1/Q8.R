07b4b7603a6fc961d41e393075d9e2096c10652a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-28 22:59:16 +0530

    Q8

---
+++ Assignment1/Q8.R
@@ -0,0 +1,9 @@
+# QUESTION :-
+# Store as a third object the values returned by omitting the first and last values of your vector from (6.). 
+
+
+# Create a new vector by omitting the first and last values of 'vec'
+remaining <- vec[-c(1, length(vec))]
+
+# Print the resulting vector
+print(remaining)
