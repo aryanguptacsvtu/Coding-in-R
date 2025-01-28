17bf1b2ae6675505f49bb190944e30d37fba42dc
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-28 22:59:33 +0530

    Q9

---
+++ Assignment1/Q9.R
@@ -0,0 +1,9 @@
+# QUESTION :-
+# Use only (7.) and (8.) to reconstruct (6.). 
+
+
+# Reconstruct the vector using 'first', 'remaining', and 'last'
+new <- c(first, remaining, last)
+
+# Print the reconstructed vector
+print(new)
