6ddaed40faf89750fb8c3dd8a00eac38cc339aa5
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-28 22:59:07 +0530

    Q7

---
+++ Assignment1/Q7.R
@@ -0,0 +1,15 @@
+# QUESTION :-
+# Extract the first and last elements of your vector from (6.), storing them as a new object. 
+
+
+# Extract the first element of the vector and store it in 'first'
+first <- vec[1]
+
+# Extract the last element of the vector and store it in 'last'
+last <- vec[length(vec)]
+
+# Print the first element
+print(first)
+
+# Print the last element
+print(last)
