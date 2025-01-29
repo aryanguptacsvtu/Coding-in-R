95ae55241ed2299801d7feae265f505074e9a6e2
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-29 23:11:14 +0530

    Q9

--- Assignment1/Q9.R
+++ Assignment1/Q9.R
@@ -7,3 +7,6 @@ new <- c(first, remaining, last)
 
 # Print the reconstructed vector
 print(new)
+# Output :-
+#[1]   3.000000   3.750000   4.500000   5.250000   6.000000   2.000000  -5.100000
+#[8] -33.000000   2.000000  -5.100000 -33.000000   2.166667
