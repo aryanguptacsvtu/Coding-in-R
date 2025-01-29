3202245b3e2e18191002a35225b3bab3497bea83
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-01-29 23:10:51 +0530

    Q6

--- Assignment1/Q6.R
+++ Assignment1/Q6.R
@@ -10,9 +10,15 @@ value <- 7/42 + 2
 
 # Print the calculated value
 print(value)
+# Output :-
+#[1] 2.166667
+
 
 # Create and store the vector in 'vec'
 vec <- c(seq(3, 6, length.out = 5), rep(c(2, -5.1, -33), times = 2), value)
 
 # Print the vector
 print(vec)
+# Output :-
+#[1]   3.000000   3.750000   4.500000   5.250000   6.000000   2.000000  -5.100000
+#[8] -33.000000   2.000000  -5.100000 -33.000000   2.166667
