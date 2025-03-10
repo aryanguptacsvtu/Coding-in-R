f3831446cc2bde702885e7b52248fa89905d611a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-03-10 23:41:20 +0530

    Q5

---
+++ Assignment3/Q5.R
@@ -0,0 +1,16 @@
+# QUESTION :-
+# Demonstrate vector recycling by creating one vector of length 4 and another of length 10, then perform an addition operation. 
+# Print the resulting vector. 
+
+
+# Create the vectors
+a <- c(1, 2, 3, 4)
+b <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+# Perform the addition operation
+result <- a + b
+
+# Print the resulting vector
+print(result)
+# Output :-
+# [1]  11  22  33  44  51  62  73  84  91 102
