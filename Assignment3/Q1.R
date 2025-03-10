2f9f8be60c94a2b87a6e0bb187a68d944e104d71
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-03-10 20:10:44 +0530

    Q1

---
+++ Assignment3/Q1.R
@@ -0,0 +1,9 @@
+# QUESTION :-
+# Write an R script to create a logical vector of length 12 that alternates between TRUE and FALSE. 
+
+
+# Create the logical vector
+logical_vec <- rep(c(TRUE, FALSE), times = 6)
+
+# Print the logical vector
+print(logical_vec)
