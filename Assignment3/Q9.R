e9b0e6995dc3b383ffe95fbec05e4b4553123eb0
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-03-11 23:45:23 +0530

    Q9

---
+++ Assignment3/Q9.R
@@ -0,0 +1,11 @@
+# QUESTION :-
+# Write R code that concatenates the strings "Data", "Science", and "R" into a single string with hyphens (-) as separators. 
+
+
+# Concatenate the strings with hyphens as separators
+my_string <- paste("Data", "Science", "R", sep = "-")
+
+# Print the concatenated string
+print(my_string)
+# Output :-
+# Data-Science-R
