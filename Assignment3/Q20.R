b46acf81469c3f2c5666fa67e3a30d37f70fca10
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2025-03-13 23:14:46 +0530

    Q20

---
+++ Assignment3/Q20.R
@@ -0,0 +1,13 @@
+# QUESTION :-
+# Write an R script that creates a string containing a newline, tab, and a backslash using escape sequences. 
+# Print the string to the console using cat(). 
+
+
+
+str <- "This is a newline:\nThis is a tab:\tThis is a backslash: \\"
+
+# Print the string using cat()
+cat(str)
+# Output :-
+# This is a newline:
+# This is a tab:	This is a backslash: \
