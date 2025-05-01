5ed9bef27f99caaca1c4439955687683962386be
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-01 10:30:13 +0530

    Vector Functions

---
+++ Vector Functions.R
@@ -0,0 +1,18 @@
+# Vector Functions in R:-
+
+rep(c(2,3,4), time=4)
+rep(c(1,4,8), each=2)
+rep(c(0,8), time =c(3,4))
+rep(1:4 , length.out =10)
+
+seq(from=3, to=13, by=2)
+seq(from=3.2, to=11.6, by=2)
+seq(from=3.5, to=1.5, by=-0.5)
+seq(from=-2.7, to=1.5, length.out=8)
+
+x <- 1:10
+any(x>5)
+any(x>9)
+any(x>10)
+all(x>0)
+all(x>5)
